15856e383bc48abe7c2f2186af5e26096468e2db
Tharathip Kulchotirat <83084032+tharathip-kulchotirat@users.noreply.github.com>
2024-02-24 03:52:16 +0000

    data_q

--- workspace/mnt/dags/weather_api_dag.py
+++ workspace/mnt/dags/weather_api_dag.py
@@ -1,67 +1,95 @@
 from airflow import DAG
-from airflow.utils import timezone
+from airflow.models import Variable
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
-from airflow.models import Variable
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils import timezone
 
-def _get_weather_data():
-    # เอา import ไว้ใน function เพราะว่ามันจะมี Process ของ Airflow ที่จะอ่าน script นี้ตลอดเวลา แปลว่ามันจะมีการ import ไปทำซ้ำๆ ตลอดเวลา ทำให้เกิดปัญหาเรื่อง memory ขึ้นมา ดังนั้นเราจึงเอาไว้ใน function แทน
-    import requests
-    from uuid import uuid4
-    import json
-    import datetime
-    
-    # Get API Key from Variable
-    api_key = Variable.get("weather_key")
-    # Get data from Weather API
-    url = f"https://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid={api_key}"
-    response = requests.get(url)
+import requests
+import json
+
+
+def _get_weather_data(**context):
+    # API_KEY = os.environ.get("WEATHER_API_KEY")
+    API_KEY = Variable.get("weather_key")
+
+    name = Variable.get("name")
+    print(f"Hello, {name}")
+
+    print(context)
+    print(context["execution_date"])
+    ds = context["ds"]
+    print(ds)
+
+    payload = {
+        "q": "bangkok",
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    url = f"https://api.openweathermap.org/data/2.5/weather"
+    response = requests.get(url, params=payload)
+    print(response.url)
+
     data = response.json()
     print(data)
-    
-    # Write data to file
-    now = datetime.datetime.now()
-    with open(f"weather_{now}.json", "w") as f:
+
+    timestamp = context["execution_date"]
+    with open(f"/opt/airflow/dags/weather_data_{timestamp}.json", "w") as f:
         json.dump(data, f)
-    
-    
-    # Prepare credentials and configuration for JSONBin API
-    x_master_key = Variable.get("x_master_key")
-    x_collection_id = Variable.get("x_collection_id")
-    api_url = "https://api.jsonbin.io/v3/b"
-    headers = {
-        "Content-Type": "application/json",
-        "X-Master-Key": x_master_key,
-        "X-Collection-Id": x_collection_id,
-        "X-Bin-Name": f"weather_{uuid4()}",
-    }
-    
-    # Read data from file
-    with open(f"weather_{now}.json", "r") as f:
-        data = json.load(f)
-    
-    # Upload data to JSONBin API
-    response = requests.post(api_url, json=data, headers=headers)
-    print(response.json())
 
+    return f"/opt/airflow/dags/weather_data_{timestamp}.json"
+
+
+def _load_data_to_postgres(**context):
+    ti = context["ti"]
+    file_name = ti.xcom_pull(task_ids="get_weather_data", key="return_value")
+    print(file_name)
+
+    pg_hook = PostgresHook(
+        postgres_conn_id="my_postgres_conn",
+        schema="postgres"
+    )
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+
+    sql = """
+        CREATE TABLE IF NOT EXISTS weathers (
+            temp FLOAT NOT NULL
+        )
+    """
+    cursor.execute(sql)
+    connection.commit()
+
+    sql = """
+        INSERT INTO weathers (temp) VALUES (31.39)
+    """
+    cursor.execute(sql)
+    connection.commit()
+
+
+default_args = {
+    "email": ["kuang.acad@gmail.com"],
+    # "retries": 1,
+}
 with DAG(
-    dag_id = "weather_api_dag",
-    start_date = timezone.datetime(2024, 2, 3),
-    schedule = "@hourly",
-    catchup = False,
-) as dag:
+    "weather_api_dag",
+    default_args=default_args,
+    schedule="@hourly",
+    start_date=timezone.datetime(2024, 2, 3),
+    catchup=False,
+):
+    start = EmptyOperator(task_id="start")
 
-    start = EmptyOperator(
-        task_id = "start"
-    )
-    
     get_weather_data = PythonOperator(
-        task_id = "get_weather_data",
-        python_callable = _get_weather_data,
+        task_id="get_weather_data",
+        python_callable=_get_weather_data,
     )
-    
-    end = EmptyOperator(
-        task_id = "end"
+
+    load_data_to_postgres = PythonOperator(
+        task_id="load_data_to_postgres",
+        python_callable=_load_data_to_postgres,
     )
-    
-    start >> get_weather_data >> end
+
+    end = EmptyOperator(task_id="end")
+
+    start >> get_weather_data >> load_data_to_postgres >> end
